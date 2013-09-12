3675f57e02ace32bd1d04d5fea2c851f7a11031f
Gunnar Ahlberg <github@gunnarahlberg.com>
2013-09-12 11:32:06 +0200

    second

---
+++ p2.pl
@@ -0,0 +1,13 @@
+/**
+* Some more knowledge
+*/
+
+/* These are facts */
+happy(yolanda).
+listens2music(mia).
+
+/** These are rules */
+/** 
+* yolanda listens to music (head)
+* is implied by / if happy (body)
+**/
